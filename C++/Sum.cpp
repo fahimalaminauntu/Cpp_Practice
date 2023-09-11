b0d9f0831a5f6295ce991b21a657965278bf7cee
auntu4672 <fahimalaminauntu@gmail.com>
2023-09-11 15:59:21 +0600

    Sum

---
+++ C++/Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // Dynamic Initialization
+    int num1 = 6, num2 = 9;
+    char ch = 'T';
+
+    int sum = num1 + num2;
+    cout << "Number one is :" << num1 << endl
+         << "Number Two is :" << num2 << endl
+         << "Character Data is:" << ch << endl
+         << "Sum is :" << sum;
+
+    return 0;
+}
