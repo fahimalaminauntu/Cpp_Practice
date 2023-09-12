72e1ea2756603138be7df51e6b92a2c8be254109
auntu4672 <fahimalaminauntu@gmail.com>
2023-09-12 19:39:33 +0600

    User input

---
+++ C++/Userinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int num1, num2, sum;
+    cout << "Enter 2 Number's : ";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << "Total Sum is : " << sum << endl
+         << "Welcome !";
+
+    getch();
+}
