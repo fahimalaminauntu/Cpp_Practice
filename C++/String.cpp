570a5c0125c0c286300d2d6c03a16f1009dfb798
auntu4672 <fahimalaminauntu@gmail.com>
2023-09-12 19:24:11 +0600

    String

---
+++ C++/String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+
+    char ch[20] = "Fahim Al-Amin Auntu";
+    cout << "Name is : " << ch;
+
+    getch();
+    return 0;
+}
