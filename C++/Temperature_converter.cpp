63713cba44b65d74467bae29b6dc818a4dc7871f
auntu4672 <fahimalaminauntu@gmail.com>
2023-09-18 18:52:35 +0600

    Temperature Converter

---
+++ C++/Temperature_converter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+
+    double farn, celsius;
+    cout << "Enter the Celsius :" << endl;
+    cin >> celsius;
+
+    farn = 1.8* celsius + 32;
+    cout << "Fahrenheit Temperature is :" << farn;
+
+    getch();
+}
